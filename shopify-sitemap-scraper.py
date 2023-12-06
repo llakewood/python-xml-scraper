7ca8029c26f8b8a6f4cff28dcc336919fef22aed
Les Lakewood <les.lakewood@gmail.com>
2023-12-05 19:33:54 -0500

    working, logs blogs to cvs

--- shopify-sitemap-scraper.py
+++ shopify-sitemap-scraper.py
@@ -4,112 +4,89 @@ from bs4 import BeautifulSoup
 import random
 import pandas as pd
 
-# Who we lookin' at? Set top level URL var
-URL = "https://blacksheepniagara.com/sitemap.xml"
+def request_xml(guid):
+  user_agents_list = [
+      'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+  ]
 
-# Fake a browser to bypass robot.txt exclusions
-user_agents_list = [
-    'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
-]
-
-# Section URL for handy usage later
-def setURL(URL = URL):
-  URL = URL
-  return URL
-
-# What is the domain root? Use it for nicename later
-def getDomain():
-  domain_name = setURL().split('/')[2]
-  return domain_name.split('.')[0]
-
-# Does list link to XML children, or HTML pages?
-def is_parent(loc):
-  loc = setURL(loc).split('.')[-1]
-  parent = 0
-  if "xml" in loc : parent = 1
-  return parent
-
-# Get XML data ready for parseing
-xml_data = requests.get(URL, headers={'User-Agent': random.choice(user_agents_list)}).content
-
-# Parse XML function
-def scrape_sitemap(xml_data):
-  # Notify terminal what site we are scraping
-  print('Scraping:' + getDomain())
-    
-  # Initializing soup variable
-    soup = BeautifulSoup(xml_data, 'xml')
-    
-  # Creating table columns to write CSV data
-    df = pd.DataFrame(columns=['loc'])    
-
-  # Iterating through loc tag and extracting text link
-    all_items = soup.find_all('loc')
-    items_length = len(all_items)
-    
-    for index, item in enumerate(all_items):
-        loc = item.text
-
-        if is_parent(loc) == 0:
-          parse_children(loc)
-        else:
-          parse_parent(loc, index, items_length)
-
-
-    return df
-
-def csv_prep(link, index, items_length, df):
-  # Adding extracted elements to rows in table
-  row = {
-      'loc': link
-  }
-  df = df._append(row, ignore_index=True)
-  print(f'Appending row %s of %s' % (index+1, items_length))
-  return 
-
-# parse_children
-def parse_parent(link, index, items_length):
-  xml_data = requests.get(link, headers={'User-Agent': random.choice(user_agents_list)}).content
-  df = scrape_sitemap(xml_data)
-  csv_prep(link, index, items_length, df)
-  
-
-# parse_children
-def parse_children(link):
-  
-  if "products" in link:
-    # results = soup.find(id="shopify-section-article-template")
-    print('is product')
-  elif "pages" in link: 
-    # results = soup.find(id="shopify-section-article-template")
-    print('is page')
-  elif "collections" in link: 
-    # results = soup.find(id="shopify-section-article-template")
-    print('is collection')
-  elif "files" in link: 
-    # results = soup.find(id="shopify-section-article-template")
-    print('is file')
-  elif "blogs" in link: 
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, "html.parser")   
-    results = soup.find(id="shopify-section-article-template")
-    blog_entries(results)
+  xml_data = requests.get(guid, headers={'User-Agent': random.choice(user_agents_list)}).content
+  return xml_data
 
 ## Extract Blog Entries
-def blog_entries(results):
+def blog_entries(index, items_length, link, results):
   if results:
     page_elements = results.find_all("article", class_="article")
     for page_content in page_elements:
-        title_element = page_content.find("h1")
-        body_content = page_content.find("div", class_="rte")
-        pub_date = page_content.find("time")
-        print(title_element.text.strip())
-        # print(body_content)
-        # print(pub_date.text.strip())
-        print()
+      # Set variables
+      title = page_content.find("h1").text.strip()
+      bodyContent = page_content.find("div", class_="rte")
+      pubDate = page_content.find("time")
 
-## Create a CSV
-df = scrape_sitemap(xml_data)
-df.to_csv(getDomain()+'.csv')
+      # Format object
+      row = {
+          'link': link,
+          'title': title,
+          'pubDate': pubDate.text.strip(),
+          'bodyContent': bodyContent
+      }
+    return bodyContent
+
+def parse_guid(index, items_length, guid, slug):
+    guid_page_content = request_xml(guid)
+    soup = BeautifulSoup(guid_page_content, "html.parser")   
+    results = soup.find(id=slug)
+    guid_data = blog_entries(index, items_length, guid, results)
+    return guid_data
+
+def parse_xml(xml_config, xml_data):
+  # Initializing soup variable
+    soup = BeautifulSoup(xml_data, 'xml')
+
+  # Creating column for table
+    df = pd.DataFrame(columns=['guid', 'title', 'pubDate', 'body'])
+
+  # Iterating through item tag and extracting elements
+    all_items = soup.find_all('url')
+    items_length = len(all_items)
+    
+    for index, item in enumerate(all_items):
+        guid = item.find('loc').text
+        image = item.find('image')
+        guid_data = parse_guid(index, items_length, guid, 'shopify-section-article-template')
+        title =  item.find('image:title')
+        body = guid_data
+        pub_date = item.find('lastmod').text
+
+       # Adding extracted elements to rows in table
+        row = {
+            'guid': guid,
+            'title': title,
+            'pubDate': pub_date,
+            'body': body
+        }
+
+        df = df._append(row, ignore_index=True)
+        print(f'Appending row %s of %s' % (index+1, items_length))
+
+    return df
+
+## Initiate scrape & write data to CSV when complete
+
+class xmlConfig:
+  guid = 'https://blacksheepniagara.com/sitemap_blogs_1.xml'
+  blog = True
+  collection = False
+  page = True
+  file = False
+
+xml_config = xmlConfig()
+
+df = parse_xml(
+  xml_config,
+  request_xml(getattr(xml_config, 'guid'))
+)
+df.to_csv('shopify.csv')
+
+
